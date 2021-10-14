3fbc0b0e7cbe722a96d125a70fbd0fad6efa3b2f
Fran Quirant Vicente <franelx01@gmail.com>
2021-10-14 16:58:56 +0200

    llave

--- seguridad/src/App.java
+++ seguridad/src/App.java
@@ -1,8 +1,12 @@
+import javax.crypto.*;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        System.out.println("Hello, World!");
-        System.out.println("Hello, skldfnsdfl!");
-
+        KeyGenerator keyGen = KeyGenerator.getInstance("AES");
+        keyGen.init(128);
+        SecretKey key = keyGen.generateKey();
+        System.out.println(keyGen);
+        System.out.println(key);
+        
     }
 }
