6466e1270efdcad300e3c1068381eb814f68cbaa
Fran Quirant Vicente <franelx01@gmail.com>
2021-10-14 17:59:08 +0200

    last

--- seguridad/src/App.java
+++ seguridad/src/App.java
@@ -1,10 +1,10 @@
 import java.io.UnsupportedEncodingException;
-import java.nio.charset.Charset;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
 
 import javax.crypto.*;
+import javax.crypto.spec.SecretKeySpec;
 
 public class App {
 
@@ -14,31 +14,34 @@ public class App {
         keyGen.init(128);
         SecretKey key = keyGen.generateKey();
         Encriptar(cadena,key);
-        Desencriptar();
+        Desencriptar(key);
     }
 
         public static String Encriptar(String cad, SecretKey key) throws NoSuchAlgorithmException, NoSuchPaddingException, UnsupportedEncodingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException{
         String resultado="";
         byte [] datosEnc;
         byte [] Encriptados;
+        byte[] keybytes;
+        keybytes = key.getEncoded();
         Cipher ObjetoCifrado =  Cipher.getInstance("AES/CBC/PKCS5Padding");
+        SecretKeySpec keySpec = new SecretKeySpec(keybytes, "AES");
 
         datosEnc = cad.getBytes("UTF-8");
-        ObjetoCifrado.init(Cipher.ENCRYPT_MODE, key);
+        ObjetoCifrado.init(Cipher.ENCRYPT_MODE, keySpec);
         Encriptados = ObjetoCifrado.doFinal(datosEnc);
         resultado = Base64.getEncoder().encodeToString(Encriptados);
         
-        System.out.println(datosEnc);
         System.out.println(resultado);
+        System.out.println(key.getAlgorithm());
 
         return resultado;
     }
 
-    public static String Desencriptar(){
+    public static String Desencriptar(SecretKey key){
         String resultado= "";
+
         
         return resultado;
     }
-
 }
 
